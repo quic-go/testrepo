88e1c283a7b74bd03a01aceaaba4418290bf56cc
Marten Seemann <martenseemann@gmail.com>
2022-11-01 12:24:11 +0000

    add a function to generate random numbers

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/marten-seemann/testrepo
 
 go 1.19
+
+require pgregory.net/rand v1.0.2
--- math.go
+++ math.go
@@ -1,5 +1,11 @@
 package testrepo
 
+import "pgregory.net/rand"
+
 func Add(a, b int) int {
 	return a + b
+}
+
+func Rand() int {
+	return rand.Int()
 }
